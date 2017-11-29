e89c281daf12d9d13e26d8f66a8bbee23e2c636e
Guga Grigolia <guga-grigolia@users.noreply.github.com>
2017-11-29 21:34:55 +0400

    change title size check to allow bigger titles

--- Classes/PHPExcel/Worksheet.php
+++ Classes/PHPExcel/Worksheet.php
@@ -429,9 +429,9 @@ class PHPExcel_Worksheet implements PHPExcel_IComparable
             throw new PHPExcel_Exception('Invalid character found in sheet code name');
         }
 
-        // Maximum 31 characters allowed for sheet title
-        if ($CharCount > 31) {
-            throw new PHPExcel_Exception('Maximum 31 characters allowed in sheet code name.');
+        // Maximum 55 characters allowed for sheet title
+        if ($CharCount > 55) {
+            throw new PHPExcel_Exception('Maximum 55 characters allowed in sheet code name.');
         }
 
         return $pValue;
@@ -451,9 +451,9 @@ class PHPExcel_Worksheet implements PHPExcel_IComparable
             throw new PHPExcel_Exception('Invalid character found in sheet title');
         }
 
-        // Maximum 31 characters allowed for sheet title
-        if (PHPExcel_Shared_String::CountCharacters($pValue) > 31) {
-            throw new PHPExcel_Exception('Maximum 31 characters allowed in sheet title.');
+        // Maximum 55 characters allowed for sheet title
+        if (PHPExcel_Shared_String::CountCharacters($pValue) > 55) {
+            throw new PHPExcel_Exception('Maximum 55 characters allowed in sheet title.');
         }
 
         return $pValue;
